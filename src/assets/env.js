efae2bba87c7fcda4ae697715286f899a2b3049d
KawserAhmedRoni <kawserahmedroni51@gmail.com>
2025-09-22 14:56:28 +0600

    start

--- src/assets/env.js
+++ src/assets/env.js
@@ -2,9 +2,9 @@
 (function (window) {
   window.__env = window.__env || {};
   //  window.__env.apiUrl = 'http://localhost/Tailoring/api/';
-  window.__env.apiUrl = "http://localhost:8082/api/";
+  window.__env.apiUrl = "http://192.168.0.100:8082/api/";
  // window.__env.fileUrl = "http://localhost:8082/";
- window.__env.fileUrl = "http://localhost:5294/";
+ window.__env.fileUrl = "http://192.168.0.100:8082/";
 
   window.__env.enableDebug = true;
   window.__env.uiVersion = "0.0.1.7";
